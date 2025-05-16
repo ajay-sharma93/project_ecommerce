d8e5769cc3a0b6ea104fa7a4d8989fcb507872c1
ajay sharma <sharma.ajaya2019@gmail.com>
2025-05-17 00:06:32 +0545

    i have created a categories screen where the list of products are added init according to the product categories

---
+++ lib/screens/components/body.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:project_ecommerce/constants.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+          child: Text("Women",style: Theme.of(context).textTheme.headlineMedium?.copyWith(fontWeight: FontWeight.bold),
+          ),
+        ),
+        Categories(),
+      ],
+    );
+  }
+}
+
+class Categories extends StatefulWidget {
+  const Categories({super.key});
+
+  @override
+  State<Categories> createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  List<String> categories = ["Hand bag","Jewellery","Footwear","Dresses"];
+
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 25,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) => buildCategory(index),
+      ),
+    );
+  }
+
+  Widget buildCategory(int index){
+    return GestureDetector(
+      onTap: (){
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      child: Padding(padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(categories[index],
+          style: TextStyle(fontWeight:FontWeight.bold,color: kTextColor,),
+          ),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.only(top: kDefaultPaddin/7),
+              height:3,
+              width: 30,
+              color:selectedIndex==index? Colors.black:Colors.transparent,
+            ),
+          ),
+        ],
+      ),
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,10 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+// ignore: unused_import
+import 'package:project_ecommerce/constants.dart';
+import 'package:project_ecommerce/screens/components/body.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,16 +17,49 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: IconButton(
-          icon: SvgPicture.asset(
-            "assets/icons/add-to-shopping-cart-e-commerce-symbol-svgrepo-com (1).svg",
-          ),
-          onPressed: () {},
+      appBar: buildAppBar(),
+      body: const Body(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset(
+          "assets/icons/back-svgrepo-com.svg",
+          height: 24,
+          width: 24,
         ),
+        onPressed: () {},
       ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: IconButton(
+            icon: SvgPicture.asset(
+              "assets/icons/search.svg",
+              height: 24,
+              width: 24,
+              color: kTextColor,
+            ),
+            onPressed: () {},
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 12.0),
+          child: IconButton(
+            icon: SvgPicture.asset(
+              "assets/icons/add-to-shopping-cart-e-commerce-symbol-svgrepo-com (1).svg",
+              height: 24,
+              width: 24,
+              color: kTextColor,
+            ),
+            onPressed: () {},
+          ),
+        ),
+      ],
     );
   }
 }
